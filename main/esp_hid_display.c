036df7abb476fd4c1d66e21980f59555cd4bb13d
Arnaud Rocher <arnaud.roche3@gmail.com>
2024-03-31 15:05:18 +0200

    LCD POC

--- main/esp_hid_display.c
+++ main/esp_hid_display.c
@@ -1,6 +1,72 @@
+#include <esp_log.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+#include <inttypes.h>
 #include <stdio.h>
+#include <sys/time.h>
+
+#include <hd44780.h>
+
+static const char* TAG = "ESP_HID_Display";
+
+static uint32_t get_time_sec()
+{
+    struct timeval tv;
+    gettimeofday(&tv, NULL);
+    return tv.tv_sec;
+}
+
+static const uint8_t char_data[] = {
+    0x04, 0x0e, 0x0e, 0x0e, 0x1f, 0x00, 0x04, 0x00,
+    0x1f, 0x11, 0x0a, 0x04, 0x0a, 0x11, 0x1f, 0x00
+};
+
+void lcd_test(void *pvParameters)
+{
+    hd44780_t lcd = {
+        .write_cb = NULL,
+        .font = HD44780_FONT_5X8,
+        .lines = 4,
+        .pins = {
+            .rs = GPIO_NUM_5,
+            .e = GPIO_NUM_3,
+            .d4 = GPIO_NUM_7,
+            .d5 = GPIO_NUM_9,
+            .d6 = GPIO_NUM_11,
+            .d7 = GPIO_NUM_12,
+            .bl = HD44780_NOT_USED,
+        }
+    };
+
+    ESP_ERROR_CHECK(hd44780_init(&lcd));
+
+    hd44780_upload_character(&lcd, 0, char_data);
+    hd44780_upload_character(&lcd, 1, char_data + 8);
+
+    hd44780_gotoxy(&lcd, 0, 0);
+    hd44780_puts(&lcd, "\x08 Hello world!");
+    hd44780_gotoxy(&lcd, 0, 1);
+    hd44780_puts(&lcd, "\x09 ");
+
+    char time[16];
+
+    while (1)
+    {
+        ESP_LOGI(TAG, "Looooooooping");
+
+        hd44780_gotoxy(&lcd, 2, 1);
+
+        snprintf(time, 7, "%" PRIu32 " ", get_time_sec());
+        time[sizeof(time) - 1] = 0;
+
+        hd44780_puts(&lcd, time);
+
+        vTaskDelay(pdMS_TO_TICKS(1000));
+    }
+
+}
 
 void app_main(void)
 {
-
+    xTaskCreate(lcd_test, "lcd_test", configMINIMAL_STACK_SIZE * 3, NULL, 5, NULL);
 }
